91f6aeec3a7737ccf66eb4b2c704c1e494fb852a
dhiya <dhiyathanus@gmail.com>
2025-08-09 03:54:43 +0530

    removed unncesary files

--- captcha.js
+++
@@ -1,6 +0,0 @@
-// Generate new CAPTCHA after successful attempt
-a = Math.floor(Math.random() * 10);
-b = Math.floor(Math.random() * 10);
-document.getElementById("math-captcha").textContent = `What is ${a} + ${b}?`;
-document.getElementById("user-answer").value = ""; // clear input
-return false; // Prevent actual submission to keep the page from reloading
